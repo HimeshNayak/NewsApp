d6115be722bb9b5624c697092106209d6c399676
HimeshNayak <himeshnayak015@gmail.com>
2021-05-15 16:12:09 +0530

    data retrieved from api

---
+++ lib/screens/homePage.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<dynamic> newsList = [];
+
+  Future<List<dynamic>> getNewsMap() async {
+    await http
+        .read(Uri.parse('https://hubblesite.org/api/v3/news'))
+        .then((value) {
+      newsList = jsonDecode(value);
+    });
+    return newsList;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: FutureBuilder<List<dynamic>>(
+            future: getNewsMap(),
+            builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+              if (snapshot.hasData) {
+                return buildNewsList(snapshot.data);
+              }
+              return CircularProgressIndicator();
+            }),
+      ),
+    );
+  }
+
+  Widget buildNewsList(List<dynamic> newsList) {
+    return ListView.builder(
+      itemCount: newsList.length,
+      itemBuilder: (context, item) {
+        return Container(
+            padding: EdgeInsets.all(10),
+            child: Text(newsList[item].toString()));
+      },
+    );
+  }
+}
